42e71b2e1427b23e9e01d6753a5db613019a85cc
victorpazb <40031882+victorpazb@users.noreply.github.com>
2019-06-25 18:52:08 -0300

    UPDATE PAG_INDEX.JS

--- js/pag_index.js
+++ js/pag_index.js
@@ -1,28 +1,28 @@
 async function login() {
-	let user = {
-		email :document.getElementById("email").value,
-		password :document.getElementById("password").value
-	}
-	
-	let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/login';
-	
-	try {
-		let response = await fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(user),
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Content-Type': 'application/json; charset=utf-8'
-			}
-		});
+  let user = {
+    email :document.getElementById("email").value,
+    password :document.getElementById("password").value
+  }
 
-		let localtoken = response.json().token;
-		alert(localtoken);
-		localStorage.setItem('token', localtoken);
-	}
-	catch (e) {
-		console.log(e);
-	}
+  let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/login';
+
+  try {
+    let response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    });
+
+    let jsonResponse = await response.json();
+    let localtoken = jsonResponse.token;
+    localStorage.setItem('token', localtoken);
+  }
+  catch (e) {
+    console.log(e);
+  }
 }
 
-document.getElementById("commit").addEventListener("click", login, false);
+document.getElementById("commit").addEventListener("click", login, false);UP
