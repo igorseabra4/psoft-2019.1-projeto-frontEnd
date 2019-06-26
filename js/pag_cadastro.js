10902ee8e33038acf79a42d12afc38c2dda1fa15
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2019-06-26 20:41:20 -0300

    atualiza código de login e cadastro

--- js/pag_cadastro.js
+++ js/pag_cadastro.js
@@ -1,41 +1,36 @@
-async function create() {
+function createUser(){
     if (document.getElementById("password").value != document.getElementById("password2").value)
         alert("A confirmação não corresponde à senha digitada.");
-    
-    let user = {
+    else create({
         firstName: document.getElementById("nome").value,
         lastName :document.getElementById("sobrenome").value,
         email :document.getElementById("email").value,
         password :document.getElementById("password").value
-    }
-    
-    let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/';
-    
-    try {
-
-        let response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json; charset=utf-8'
-              }
-        });
-    
-        if (response.status == 201)
-            window.location = 'index.html';
-        else{
-        let error = await response.json();
-        console.log(error);
-        }
-    }
-    catch (ex) { 
-        console.log(ex);
-    };
+    });
 }
 
-function createUser(){
-    create();
+async function create(user) {
+    let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/';
+    
+    await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    })
+    .then(response => {
+        if (response.status == 201){
+            alert("Usuário criado com sucesso.");
+            window.location = 'index.html';
+        }
+        else {
+            let error = await response.json();
+            alert(error.message);
+        }
+    })
+    .catch(e => console.log(e));
 }
 
 document.getElementById("createUserButton").addEventListener("click", createUser, false);
--- js/pag_index.js
+++ js/pag_index.js
@@ -1,32 +1,33 @@
 function login() {
-  let user = {
-    email :document.getElementById("email").value,
-    password :document.getElementById("password").value
-  }
-
-  sendLogin(user);
-}
-
-async function sendLogin(user) {
-  let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/login';
-
-  try {
-    let response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(user),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json; charset=utf-8'
-      }
+    sendLogin({
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
     });
-
-    let jsonResponse = await response.json();
-    let localtoken = jsonResponse.token;
-    localStorage.setItem('token', localtoken);
-  }
-  catch (e) {
-    console.log(e);
-  }
 }
 
-document.getElementById("commit").addEventListener("click", login, false);
+async function sendLogin(user){
+    let url = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/login';
+    
+    await fetch(url, {
+        method:'POST',
+        body:JSON.stringify(user),
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type':'application/json;charset=utf-8'
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (response.status == 200) {
+            alert("Login realizado com sucesso.");
+            window.location = 'index.html';
+        } else {
+            localStorage.removeItem('token');
+            alert(json.message);
+        }
+    })
+    .catch(e => console.log(e));
+}
+
+document.getElementById("commit").addEventListener("click",login,false);
+    
