92eb18bbf5496b1dabd24d9470ce442da06aec07
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2019-06-28 02:02:28 -0300

    adiciona perfil e likes

--- js/course_interactions.js
+++ js/course_interactions.js
@@ -12,17 +12,231 @@ function getVariable(variable)
     }
 }
 
-function init(){
+function darErro(){
+    alert("Houve um erro ao fazer a requisição. Por favor, faça login e tente novamente.");
+    
+    localStorage.removeItem('token');
+    localStorage.removeItem('userID');
+    window.location = 'index.html';
+}
+
+async function getCourseProfile(discID) {
+    let data;
+
+    let response = await fetch(urlbase + discID, {
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+    });
+    
+    let json = await response.json();
+    
+    if (response.status == 200)
+        data = json;
+    else
+    {
+        darErro();
+        data = undefined;
+    }
+    
+    return data;
+}
+
+// like functions
+
+async function like_click() {
+    let discID = getVariable('discID');
+    let userID = parseInt(localStorage.getItem('userID'));
+    let profile = await getCourseProfile(discID);
+
+    let likeCount = profile.likeCount;
+    if (profile.likedUserIDs.includes(userID)) {
+        console.log('killlike');
+        removeLike(discID, userID);
+        set_button_notliked();
+        likeCount--;
+    }
+    else {
+        console.log('putlike');
+        putLike(discID, userID);
+        set_button_liked();
+        likeCount++;
+    }
+
+    document.getElementById("likecounter").innerHTML = `<h1 id="likecounter">Número de likes: ${likeCount}</h1>`;
+}
+
+document.getElementById("meulike").addEventListener("click", like_click, false);
+
+function set_button_liked() {
+    document.getElementById("meulike").innerHTML = `<button id="meulike">Clique para remover like</button>`
+}
+
+function set_button_notliked() {
+    document.getElementById("meulike").innerHTML = `<button id="meulike">Clique para dar like</button>`
+}
+
+async function putLike(discID, userID) {
+    await fetch(urlbase + discID + '/like', {
+        method:'PUT',
+        body: JSON.stringify(userID),
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type':'application/json;charset=utf-8',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    })
+    .then(response => {
+        if (response.status != 200) 
+            return {success: false, json: response.json()};
+        return {success: true};
+    })
+    .then(resp => {
+        if (!resp.success)
+            darErro();
+            //alert(resp.json.message);
+    })
+    .catch(e => console.log(e));
+}
+
+async function removeLike(discID, userID) {
+    await fetch(urlbase + discID + '/like', {
+		method: 'DELETE',
+        body: JSON.stringify(userID),
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type':'application/json;charset=utf-8',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    })
+    .then(response => {
+        if (response.status != 200) 
+            return {success: false, json: response.json()};
+        return {success: true};
+    })
+    .then(resp => {
+        if (!resp.success)
+            darErro();
+            //alert(resp.json.message);
+    })
+    .catch(e => console.log(e));
+}
+
+
+// grade functions
+
+async function putGrade(discID, userID, grade) {
+    await fetch(urlbase + discID + '/grade', {
+        method:'PUT',
+        body: JSON.stringify({
+            'userID': userID,
+            'grade': grade
+        }),
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type':'application/json;charset=utf-8',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    })
+    .then(response => {
+        if (response.status != 200) 
+            return {success: false, json: response.json()};
+        return {success: true};
+    })
+    .then(resp => {
+        if (!resp.success)
+            darErro();
+            //alert(resp.json.message);
+    })
+    .catch(e => console.log(e));
+}
+
+// comment functions
+
+async function putComment(discID, userID, comment, parentCommentID, date) {
+    let data;
+    await fetch(urlbase + discID + '/comment', {
+		method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            'userID': userID,
+            'comment': comment,
+            'parentCommentID': parentCommentID,
+            'date': date
+        })
+	})
+    .then(result => data = treatResult(result))
+    .catch(e => console.log(e));
+    
+    return data;
+}
+
+async function removeComment(discID, userID, commentID) {
+    let data;
+    await fetch(urlbase + discID + '/like', {
+		method: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            'userID': userID,
+            'commentID': commentID
+        })
+	})
+    .then(result => data = treatResult(result))
+    .catch(e => console.log(e));
+    
+    return data;
+}
+
+async function usersWithId(ids) {
+    let data;
+    await fetch('https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/withID/', {
+        body: JSON.stringify(ids)
+    })
+    .then(response => response.json())
+    .then(result => data = treatResult(result))
+    .catch(e => console.log(e));
+    
+    return data;
+}
+
+
+// main functions
+
+async function init(){
     try {
-        let discID = getVariable(discID);
+        let discID = getVariable('discID');
 
         localStorage.setItem('currentCourseID', discID);
     
-        let courseProfile = getCourseProfile(discID);
+        let profile = await getCourseProfile(discID);
+        
+        let $perfil = document.getElementById("disciplina");
 
-        // prencher html da página aqui!
+        if (profile.likedUserIDs.includes(parseInt(localStorage.getItem('userID'))))
+            set_button_liked();
+        else
+            set_button_notliked();
 
-        // o courseProfile retornado acima é um objeto com os atributos:
+        $perfil.innerHTML = `<div id="disciplina">`;
+        $perfil.innerHTML += `<h1>ID: ${profile.id}</h1>`;
+        $perfil.innerHTML += `<h1>Nome: ${profile.name}</h1>`;
+        $perfil.innerHTML += `<h1 id="likecounter">Número de likes: ${profile.likeCount}</h1>`;
+        $perfil.innerHTML += `<h1>Nota: ${profile.grade}</h1>`;
+        $perfil.innerHTML += `</div>`;
+
+        let $comments = document.getElementById("comentarios");
+
+        $comments.innerHTML = `<div id="comentarios">`;
+        profile.comments.forEach(element => {
+            $comments.innerHTML += `<h1>Comentário</h1>`;
+        });
+        $comments.innerHTML += `</div>`;
+        
+        // o profile retornado acima é um objeto com os atributos:
         // id (discID)
         // name (nome da disciplina)
         // likedUserIDs (IDs de usuários que deram like)
@@ -41,93 +255,8 @@ function init(){
     }
     catch (e) {
         // fazer alguma coisa com o erro
+        console.log(e);
     }
 }
 
 init();
-
-async function getCourseProfile(discID) {
-    return (await fetch(urlbase + discID, {
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-	})
-    .then(result => result.json())
-    .catch(e => console.log(e)));
-}
-
-async function putLike(discID, userID) {
-    await fetch(urlbase + discID + '/like', {
-		method: 'PUT',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: userID
-    })
-    .catch(e => console.log(e));
-}
-
-async function removeLike(discID, userID) {
-    await fetch(urlbase + discID + '/like', {
-		method: 'DELETE',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: userID
-	})
-    .catch(e => console.log(e));
-}
-
-async function putGrade(discID, userID, grade) {
-    await fetch(urlbase + discID + '/grade', {
-		method: 'PUT',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            'userID': userID,
-            'grade': grade
-        })
-	})
-    .catch(e => console.log(e));
-}
-
-async function putComment(discID, userID, comment, parentCommentID, date) {
-    await fetch(urlbase + discID + '/comment', {
-		method: 'PUT',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            'userID': userID,
-            'comment': comment,
-            'parentCommentID': parentCommentID,
-            'date': date
-        })
-	})
-    .catch(e => console.log(e));
-}
-
-async function removeComment(discID, userID, commentID) {
-    await fetch(urlbase + discID + '/like', {
-		method: 'DELETE',
-        headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            'userID': userID,
-            'commentID': commentID
-        })
-	})
-    .catch(e => console.log(e));
-}
-
-// ids deve ser uma lista de numeros que correspondem a ids de usuarios
-// a funcao retorna a lista dos nomes de usuarios desses ids
-async function usersWithId(ids) {
-    return await fetch('https://projeto-psoft-igor-victor.herokuapp.com/api/v1/auth/withID/', {
-        body: ids
-    })
-    .then(response => response.json())
-    .catch(e => console.log(e));
-}
--- js/pag_disciplinas.js
+++ js/pag_disciplinas.js
@@ -1,13 +1,15 @@
-let urlbase = 'http://projeto-psoft-igor-victor.herokuapp.com/api/v1/courses';
+let urlbase = 'https://projeto-psoft-igor-victor.herokuapp.com/api/v1/courses';
 
 async function get_disciplinas() {
     await fetch(urlbase)
     .then(response => response.json())
     .then(data => preenche_tabela(data));
 }
+
 // funcao usada para pesquisar as disciplinas digitando seu nome ou parte dele na barra de busca
 async function get_disciplinas_substring() {
     let str = document.getElementById("txtBusca").value;
+
     await fetch(urlbase + '/substring?str=' + str)
     .then(response => response.json())
     .then(data => preenche_tabela(data));
@@ -25,28 +27,19 @@ async function get_disciplinas_ranking() {
 }
 
 function preenche_tabela(disciplinas){
-    
-    if (disciplinas) {
-
-    }
     let $disciplinas = document.getElementById("disciplinas");
     $disciplinas.innerHTML =
     `<table id="table">
-    
     <div class="header-row row">
         <span class="cell primary">ID</span>
         <span class="cell">NOME</span> 
     </div>`;
 	disciplinas.forEach(disc => {
-        
 		$disciplinas.innerHTML += 
-        `
-        <div class="row">
-            <input type="radio" name="expand">
+        `<div class="row">
             <span class="cell primary" data-label="ID">${disc.id}</span>
-            <span class="cell" data-label="NOME">${disc.name}</span>
+            <span class="cell" data-label="NOME"><a href="perfil.html?discID=${disc.id}">${disc.name}</a></span>
         </div>`;
-
     });
     $disciplinas.innerHTML += `</table>`;
 }
