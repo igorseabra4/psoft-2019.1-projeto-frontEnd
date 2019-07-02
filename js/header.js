a7e0b5c42e4993b81b6ab2ef1d5b681ed46d64e5
victorpazb <victor.braga@ccc.ufcg.edu.br>
2019-07-02 20:12:14 -0300

    logout ok

--- js/header.js
+++ js/header.js
@@ -4,11 +4,12 @@ let buttonHtml = `<button id="botao-logout" onclick="logout()" class="botao-comu
 
 if (userName !== null && userName !== undefined)
     document.getElementById("username-header").innerHTML =
-    `<span id="username-span">Usuário: ${userName}</span>
+    `<span id="username-div">Usuário: ${userName}</span>
     ${buttonHtml}`;
 else
     document.getElementById("username-header").textContent = '';
 
+// funcao logout() usada para deslogar o usuario quando passar da fase de login
 function logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('userName');
